d712ee2346899b92d54a2d2f06763e22520e53b0
Bryan2503 <bryanmergar@gmail.com>
2024-06-10 10:27:42 -0600

    Update ble_switch_handler.cpp

--- components/esp32_ble_controller/ble_switch_handler.cpp
+++ components/esp32_ble_controller/ble_switch_handler.cpp
@@ -13,7 +13,9 @@ namespace esp32_ble_controller {
 static const char *TAG = "ble_switch_handler";
 
 void BLESwitchHandler::on_characteristic_written() {
+  ESP_LOGD(TAG, "on_characteristic_written called");
   std::string value = get_characteristic()->getValue();
+  ESP_LOGD(TAG, "Characteristic value: %s", value.c_str());
   if (!value.empty()) {
     uint8_t on = std::stoi(value);
     ESP_LOGD(TAG, "Switch chracteristic written: %d", on);
@@ -25,8 +27,10 @@ void BLESwitchHandler::on_characteristic_written() {
     // Store the value in the text_sensor
     auto on_value = id(on_value_text_sensor);
     on_value->publish_state(std::to_string(on));
+    ESP_LOGD(TAG, "Published state to on_value_text_sensor");
   }
 }
+
 
 
 } // namespace esp32_ble_controller
