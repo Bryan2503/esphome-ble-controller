a9c95476b4ec72da82a15e687091093bf8ddce36
Bryan2503 <bryanmergar@gmail.com>
2024-06-10 10:58:12 -0600

    Update ble_switch_handler.cpp

--- components/esp32_ble_controller/ble_switch_handler.cpp
+++ components/esp32_ble_controller/ble_switch_handler.cpp
@@ -1,37 +1,44 @@
 #include "ble_switch_handler.h"
 #include "esphome.h"  // Include esphome.h
 
+#pragma once
+
+#include "esphome/core/defines.h"
 #ifdef USE_SWITCH
 
-#include "esphome/core/log.h"
-#include "esphome/components/global/global_string.h"  // Include the global_string component
+#include <string>
+#include "esphome.h"
+#include "esphome/components/global/global.h"
+#include "esphome/components/switch/switch.h"
+#include "ble_component_handler.h"
 
-using namespace esphome;  // Use the esphome namespace
+using std::string;
 
+namespace esphome {
 namespace esp32_ble_controller {
 
-static const char *TAG = "ble_switch_handler";
+using switch_::Switch;
 
-void BLESwitchHandler::on_characteristic_written() {
-  ESP_LOGD(TAG, "on_characteristic_written called");
-  std::string value = get_characteristic()->getValue();
-  ESP_LOGD(TAG, "Characteristic value: %s", value.c_str());
-  if (!value.empty()) {
+class BLESwitchHandler : public BLEComponentHandler<Switch> {
+public:
+  BLESwitchHandler(Switch* component, const BLECharacteristicInfoForHandler& characteristic_info) : BLEComponentHandler(component, characteristic_info) {}
+  virtual ~BLESwitchHandler() {}
+
+protected:
+  virtual bool can_receive_writes() { return true; }
+  virtual void on_characteristic_written() override {
+    // ...
     uint8_t on = std::stoi(value);
-    ESP_LOGD(TAG, "Switch chracteristic written: %d", on);
-    if (on)
-      get_component()->turn_on();
-    else
-      get_component()->turn_off();
+    // ...
 
-    // Store the value in the global
-    auto on_value_global = global_string::GlobalsComponent::get_global_by_id("on_value_global");
-    on_value_global->value() = std::to_string(on);
-    ESP_LOGD(TAG, "Published state to on_value_global");
+    // Asigna el valor a la variable global
+    auto switch_value = global::GlobalsComponent::get_global_by_id("switch_value");
+    switch_value->value() = on;
   }
-}
+};
 
 } // namespace esp32_ble_controller
+} // namespace esphome
 
 #endif
 
